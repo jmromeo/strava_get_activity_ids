4c20e56c78d93629bb0c763aec5086fe1ca4747e
Joseph Romeo <jromeo1982@gmail.com>
2021-03-24 17:07:27 -0700

    Added activity name and type filtering capability

--- src/strava_activity_id_by_name.py
+++ src/strava_activity_id_by_name.py
@@ -3,6 +3,7 @@ import argparse
 
 activity_col = 0
 name_col = 2 
+type_col = 3
 
 def parse_arguments():
 	parser = argparse.ArgumentParser(description='Get list of activity IDs with matching name and activity type')
@@ -21,21 +22,48 @@ def parse_arguments():
 						required=False, 
 						type=str,
 						help="(Optional) Type of activity to search")
+	parser.add_argument('-pid', '--print_id', 
+						dest='print_id', 
+						required=False, 
+						action='store_true',
+						help="(Optional) Print only matching activity IDs and no other information")
 
 	return parser.parse_args()
 
+def find_matching_ids(activity_file, activity_name, activity_type):
+	activity_ids = []
+
+	with open(activity_file, newline='') as activityfile:
+		activities = csv.reader(activityfile, delimiter=',')
+		for row in activities:
+			check_name = False if (activity_name == None) else True
+			check_type = False if (activity_type == None) else True
+			name_match = None if (activity_name == None) else (str.lower(activity_name) in str.lower(row[name_col]))
+			type_match = None if (activity_type == None) else (str.lower(activity_type) in str.lower(row[type_col]))
+
+			if check_name and check_type:
+				if name_match and type_match:
+					activity_ids.append((row[activity_col], row[name_col], row[type_col]))
+
+			elif check_name:
+				if name_match:
+					activity_ids.append((row[activity_col], row[name_col], row[type_col]))
+
+			elif check_type:
+				if type_match:
+					activity_ids.append((row[activity_col], row[name_col], row[type_col]))
+
+			else:
+				activity_ids.append((row[activity_col], row[name_col], row[type_col]))
+
+	return activity_ids
+
 args = parse_arguments()
-print(args)
 
-with open(args.activity_file.name, newline='') as activityfile:
-	activities = csv.reader(activityfile, delimiter=',')
-	row_num = 0
-	for row in activities:
-		row_num = row_num + 1
-		col_num = 0
-		print(f"{row[activity_col]}: {row[name_col]}")
-		print(f"{row_num}")
-		for col in row:
-			print(f"    {col_num}: {col},")
-			col_num=col_num + 1
+activity_ids = find_matching_ids(args.activity_file.name, args.activity_name, args.activity_type)
+for activity in activity_ids:
+	if args.print_id != None:
+		print(activity[0])
 
+	else:
+		print(activity)
